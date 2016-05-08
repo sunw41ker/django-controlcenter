98f1d62a5a0137a2a3d5dd6f233c5e97227b6eb0
Murad Byashimov <byashimov@gmail.com>
2016-05-08 14:28:34 +0300

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 EXCLUDE_FROM_PACKAGES = ['controlcenter.stylus',
                          'controlcenter.images']
 
