94d22d6e90f287438dc01ca6087db26b81397e45
Tony S Yu <tsyu80@gmail.com>
2018-08-26 12:34:46 -0500

    Revert "Add example project for easy demo of widgets"
    This reverts commit e53606582ea6d4fcc303d986934eebf9407695d4.

--- example_project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from example_project import models
-
-
-admin.site.register(models.Pizza)
-admin.site.register(models.Restaurant)
-admin.site.register(models.Order)
--- example_project/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExampleProjectConfig(AppConfig):
-
-    name = 'example_project'
