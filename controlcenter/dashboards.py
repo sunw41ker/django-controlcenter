49ca8dbfe680046aaa5e9915338d2b20178e5ecb
sunw41ker <5059878+sunw41ker@users.noreply.github.com>
2019-12-07 02:33:13 +0300

    Update dashboards.py

--- controlcenter/dashboards.py
+++ controlcenter/dashboards.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from django.core.urlresolvers import reverse
 from django.forms.widgets import MediaDefiningClass
-from django.utils import six
+from import six
 
 from . import app_settings
 from .base import BaseModel
